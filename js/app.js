6294ec9a9e620a2583b347fb8471b1c94f89b590
yichen123 <chenyi330@hotmail.com>
2015-06-21 09:32:00 +0800

    the background appears

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 10;
+    this.y = 50;
+    this.speed = 10;
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +28,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function() {
+
+}
+
+Player.prototype.render = function() {
+
+}
+
+Player.prototype.handleInput = function() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
 
+
+var player = new Player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
