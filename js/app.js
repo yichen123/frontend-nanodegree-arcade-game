927d38c62f3b4e304a5f6473855c7c7a93db9c9d
yichen123 <chenyi330@hotmail.com>
2015-06-22 23:27:52 +0800

    reconstruct code
    Adding entity class and put enemy and player under inheritance classes
    of entity.
    removed unnecessary constant
    add ifCollision(object) to Player that detects if player touch anything
    moving position picking method outside the constructor

--- js/app.js
+++ js/app.js
@@ -1,72 +1,64 @@
 'use strict';
 
 //constants
-var GRIDHEIGHT = 85;
-var GRIDWIDTH = 100;
-var HEIGHT = 400;
-var WIDTH = 500;
-var ENEMYHEIGHT = 60;
-var ENEMYWIDTH = 100;
-var PLAYERWIDTH = 60;
+var GRIDHEIGHT = 83;
+var GRIDWIDTH = 101;
+var NUMROWS = 6;
+var NUMCOLS = 5;
+var HEIGHT = GRIDHEIGHT * (NUMROWS - 1);
+var WIDTH = GRIDWIDTH * NUMCOLS;
 
-var Enemy = function() {
-    // This is the enemy object
-    this.sprite = 'images/enemy-bug.png';
-    var randomPick = Math.round(Math.random() * 2 + 1);
-    if (randomPick === 1 || randomPick === 3) {
-        this.x = -ENEMYWIDTH;
-        this.direction = 1;
-    } else {
-        this.x = WIDTH;
-        this.direction = -1;
-    }
-    this.y = randomPick * GRIDHEIGHT - ENEMYHEIGHT / 2;
-    this.speed = (100 * Math.random() + 150) * this.direction;
-    console.log(this.x, this.y, this.speed);
+var Entity = function(x, y) {
+    // This is the entity object that the
+    // father class for both enemy and player
+    this.x = x;
+    this.y = y;
 };
+
+Entity.prototype.render = function() {
+    // drawing function for the entity
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// This is the enemy object
+var Enemy = function(x, y, speed) {
+    Entity.call(this, x, y);
+    this.sprite = 'images/enemy-bug.png';
+    this.speed = speed;
+};
+Enemy.prototype = Object.create(Entity.prototype);
 
 Enemy.prototype.update = function(dt) {
     // Update the enemy's position, required method for game
     // Make sure the enemy loop through the table
-    if (this.x > WIDTH && this.direction === 1) {
-        this.x = -ENEMYWIDTH;
-    } else if (this.x < -ENEMYWIDTH && this.direction === -1) {
+    if (this.x > WIDTH && this.speed > 0) {
+        this.x = -GRIDWIDTH;
+    } else if (this.x < -GRIDWIDTH && this.speed < 0) {
         this.x = WIDTH;
     } else {
         this.x = this.x + this.speed * dt;
     }
 };
 
-Enemy.prototype.render = function() {
-    // drawing function for the enemy
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+var Player = function(x, y) {
+    // This is the player object
+    Entity.call(this, x, y);
+    this.sprite = 'images/char-boy.png';
 };
 
-var Player = function() {
-    // This is the player object
-    this.sprite = 'images/char-boy.png';
-    this.x = 0;
-    this.y = HEIGHT;
-};
+Player.prototype = Object.create(Entity.prototype);
 
 Player.prototype.update = function() {
     // reposition the player when it reaches top or if it touches with any
     // of the enemys.
     if (this.y < 0) {
-        this.y = HEIGHT;
+        this.y = HEIGHT - GRIDHEIGHT / 2;
     }
     for (var enemy in allEnemies) {
-        if (Math.abs(allEnemies[enemy].y - this.y) < 10) {
-            if (Math.abs(allEnemies[enemy].x - this.x) < (ENEMYWIDTH + PLAYERWIDTH) / 2) {
-                this.y = HEIGHT;
-            }
+        if (this.ifCollision(allEnemies[enemy]) === true) {
+            this.y = HEIGHT - GRIDHEIGHT / 2;
         }
     }
-};
-
-Player.prototype.render = function() {
-    //drawing function for the player
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.handleInput = function(input) {
@@ -80,19 +72,37 @@ Player.prototype.handleInput = function(input) {
     } else if (input === 'down' && this.y <= HEIGHT - GRIDHEIGHT) {
         this.y += GRIDHEIGHT;
     }
+};
 
+Player.prototype.ifCollision = function(object) {
+    // detect if player touches the ojbect
+    var ifTouch = false;
+    if (this.y === object.y && Math.abs(object.x - this.x) < GRIDWIDTH / 2) {
+        ifTouch = true;
+    }
+    return ifTouch;
 };
 
 // a collection of enemies
 var allEnemies = [];
 
-// adding enemy to the collection
+// generate enemies randomly and add them into the collection
 while (allEnemies.length < 10) {
-    allEnemies.push(new Enemy());
+    var randomPick = Math.round(Math.random() * 2 + 1);
+    if (randomPick === 1 || randomPick === 3) {
+        var x = -GRIDWIDTH;
+        var direction = 1;
+    } else {
+        var x = WIDTH;
+        var direction = -1;
+    }
+    var y = randomPick * GRIDHEIGHT - GRIDHEIGHT / 2;
+    var speed = (100 * Math.random() + 150) * direction;
+    allEnemies.push(new Enemy(x, y, speed));
 }
 
 // this is the player
-var player = new Player();
+var player = new Player(WIDTH / 2 - GRIDWIDTH / 2, HEIGHT - GRIDHEIGHT / 2);
 
 // key listener that waits for triggering the keyHandler
 document.addEventListener('keyup', function(e) {
